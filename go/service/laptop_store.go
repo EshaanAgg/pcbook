d7bd0be8e07bfc53ea8085ccd05d9402b32c9a99
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2023-09-23 17:31:30 +0530

    Add a server streaming function to the server

--- go/service/laptop_store.go
+++ go/service/laptop_store.go
@@ -15,6 +15,8 @@ var ErrAlreadyExists = errors.New("record already exists in the store")
 type LaptopStore interface {
 	Save(laptop *pb.Laptop) error
 	Find(id string) (*pb.Laptop, error)
+	// A function to search for laptops with a filter, and returns each laptop one-by-one with the found callback function
+	Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error
 }
 
 type InMemoryLaptopStore struct {
@@ -38,10 +40,9 @@ func (store *InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
 		return ErrAlreadyExists
 	}
 
-	other := &pb.Laptop{}
-	err := copier.Copy(other, laptop)
+	other, err := deepCopy(laptop)
 	if err != nil {
-		return fmt.Errorf("cannot copy the laptop data: %w", err)
+		return err
 	}
 
 	store.data[other.Id] = other
@@ -57,6 +58,68 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 		return nil, nil
 	}
 
+	return deepCopy(laptop)
+}
+
+func (store *InMemoryLaptopStore) Search(filter *pb.Filter, found func(laptop *pb.Laptop) error) error {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	for _, laptop := range store.data {
+		if isQualified(filter, laptop) {
+			other, err := deepCopy(laptop)
+			if err != nil {
+				return err
+			}
+
+			err = found(other)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func isQualified(filter *pb.Filter, laptop *pb.Laptop) bool {
+	if laptop.GetPriceUsd() > filter.GetMaxPriceUsd() {
+		return false
+	}
+	if laptop.GetCpu().GetNumberCores() < filter.GetMinCpuCores() {
+		return false
+	}
+	if laptop.GetCpu().GetMinGhz() < filter.GetMinCpuGhz() {
+		return false
+	}
+	if toBit(laptop.GetRam()) < toBit(filter.GetMinRam()) {
+		return false
+	}
+	return true
+}
+
+func toBit(mem *pb.Memory) uint64 {
+	value := mem.GetValue()
+
+	switch mem.GetUnit() {
+	case pb.Memory_BIT:
+		return value
+	case pb.Memory_BTYE:
+		return value << 3
+	case pb.Memory_KILOBYTE:
+		return value << 13
+	case pb.Memory_MEGABYTE:
+		return value << 23
+	case pb.Memory_GIGABYTE:
+		return value << 33
+	case pb.Memory_TERABYTE:
+		return value << 43
+	}
+
+	return 0
+}
+
+func deepCopy(laptop *pb.Laptop) (*pb.Laptop, error) {
 	other := &pb.Laptop{}
 	err := copier.Copy(other, laptop)
 	if err != nil {
@@ -64,5 +127,4 @@ func (store *InMemoryLaptopStore) Find(id string) (*pb.Laptop, error) {
 	}
 
 	return other, nil
-
 }
